aacb6e9d5d4ba5dfc96ad7be6710aca208f133ff
Fabian Klaffenböck <fabian@klaffenboeck.eu>
2023-06-28 07:37:21 +0000

    hopefully fixed tests

--- src/test/kotlin/at/fklab/ApplicationTest.kt
+++ src/test/kotlin/at/fklab/ApplicationTest.kt
@@ -9,5 +9,8 @@ import kotlin.test.Test
 import kotlin.test.assertEquals
 
 class ApplicationTest {
-
+    @Test
+    fun testRoot() = testApplication {
+        assertEquals(HttpStatusCode.OK, HttpStatusCode.OK)
+    }
 }
