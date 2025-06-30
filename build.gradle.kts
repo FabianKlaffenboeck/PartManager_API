8a442e82090aa4273981d88272a504d0ade0e57b
Fabian Klaffenböck <fabian@klaffenboeck.eu>
2025-06-30 19:47:02 +0200

    needed to rename the file for whatever reason

--- build.gradle.kts
+++ build.gradle.kts
@@ -24,6 +24,7 @@ tasks.jacocoTestReport {
 
     reports {
         xml.required.set(true)
+        xml.outputLocation.set(file("$buildDir/reports/jacoco/test/jacoco.xml"))
         html.required.set(true)
         csv.required.set(false)
     }
