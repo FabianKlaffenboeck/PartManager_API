3ccb13b2002157fec0eb998c0934a815d05ba8d6
Fabian Klaffenböck <fabian@klaffenboeck.eu>
2025-06-30 21:16:34 +0200

    updated dependencies

--- build.gradle.kts
+++ build.gradle.kts
@@ -26,7 +26,7 @@ tasks.jacocoTestReport {
     reports {
         xml.required.set(true)
         html.required.set(true)
-//        csv.required.set(false)
+        csv.required.set(false)
     }
 }
 
